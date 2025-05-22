21e7ae90431c52c55af33876c0f6c8ec1b9e3b1e
Martin Elsaesser <marels@aon.at>
2025-05-22 20:16:59 +0200

    improve typescript loader example

--- typescript-loader/main.ts
+++ typescript-loader/main.ts
@@ -1,3 +1,6 @@
-const sum: number = 10 + 2;
+import { b } from "./modules/js-module.js";
+import { a } from "./modules/ts-module.ts";
+
+const sum: number = a + b;
 
 console.log(`The sum is: ${sum}`);
