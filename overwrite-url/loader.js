3aacf85fb8e7d45e11e10fe5df2c73f4e9a7d0f2
Martin Elsaesser <marels@aon.at>
2025-05-22 19:31:02 +0200

    minimal loader example

---
+++ overwrite-url/loader.js
@@ -0,0 +1,11 @@
+import fs from "node:fs/promises";
+
+const overwrites = JSON.parse(await fs.readFile("overwrites.json", "utf-8"));
+
+export async function resolve(specifier, context, nextResolve) {
+  if (specifier in overwrites) {
+    return await nextResolve(overwrites[specifier], context);
+  }
+
+  return await nextResolve(specifier, context);
+}
